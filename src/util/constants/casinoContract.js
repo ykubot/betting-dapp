f7bcadeb05e6e5e4910399c04422dd9520723fce
窪田雄一 <kubota@curicle.jp>
2018-03-13 18:37:51 +0900

    Base app

---
+++ src/util/constants/casinoContract.js
@@ -0,0 +1,74 @@
+const address = '0x76c3128aa6a9ae77dfdc72f4a527878c27b8fc3b'
+const ABI = [{
+    "constant": false,
+    "inputs": [],
+    "name": "kill",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "checkContractBalance",
+    "outputs": [{
+      "name": "",
+      "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [{
+      "name": "_number",
+      "type": "uint256"
+    }],
+    "name": "bet",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [{
+        "name": "_minBet",
+        "type": "uint256"
+      },
+      {
+        "name": "_houseEdge",
+        "type": "uint256"
+      }
+    ],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "constructor"
+  },
+  {
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "fallback"
+  },
+  {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": false,
+        "name": "_status",
+        "type": "bool"
+      },
+      {
+        "indexed": false,
+        "name": "_amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "Won",
+    "type": "event"
+  }
+]
+export {
+  address,
+  ABI
+}
